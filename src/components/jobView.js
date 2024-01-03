66175da76b8110c2f8fb4a356882bfa12dda2c70
iRobot90 <amiandajesse27@gmail.com>
2024-01-03 23:18:17 +0300

    imported sessionProvider

---
+++ src/components/jobView.js
@@ -0,0 +1,13 @@
+// components/JobView.js
+import React from "react";
+
+const JobView = () => {
+  // Fetch jobs data and implement filter logic
+  return (
+    <div>
+      <h3>Available Jobs</h3>
+    </div>
+  );
+};
+
+export default JobView;
--- src/pages/jobseeker/dashboard.js
+++ src/pages/jobseeker/dashboard.js
@@ -1,9 +1,21 @@
+// pages/dashboard.js
+import React from "react";
+import { useSession } from "next-auth/react";
+import JobView from "../../components/jobView";
+
 const Dashboard = () => {
-    return (
-        <div>
-            <h2>Dashboard</h2>
-        </div>
-    )
-}
+  const { data: session } = useSession();
+
+  if (!session) {
+    return <p>You need to sign in first</p>;
+  }
+
+  return (
+    <div>
+      <h1>Welcome to your dashboard, {session.user.name}!</h1>
+      <JobView />
+    </div>
+  );
+};
 
 export default Dashboard;
