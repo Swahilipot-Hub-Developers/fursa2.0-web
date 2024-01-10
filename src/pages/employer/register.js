7e722613130279c622ac2e0cf2a2b4438d9d1892
rndinda <rndinda73@gmail.com>
2024-01-10 13:03:10 +0300

    Registration for Seeker and employer

--- src/pages/employer/register.js
+++ src/pages/employer/register.js
@@ -1,7 +1,59 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import axios from 'axios';
+import { useState } from 'react';
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        user: {
+            username: '',
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone_number: '',
+            password: '',
+        },
+        sub_county: '',
+        company_name: '',
+        company_description: '',
+        company_email: '',
+        company_phone: '',
+        company_registration_number: '',
+        company_type: '',
+    });
+
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        const fieldId = id.split('.')[0];
+    
+        if (fieldId === 'user') {
+            const subFieldId = id.split('.')[1];
+            setFormData((prevFormData) => ({
+                ...prevFormData,
+                user: {
+                    ...prevFormData.user,
+                    [subFieldId]: value,
+                },
+            }));
+        } else {
+            setFormData((prevFormData) => ({
+                ...prevFormData,
+                [id]: value,
+            }));
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            console.log(formData);
+            const response = await axios.post('http://127.0.0.1:8000/api/auth/register/employer/', formData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div>
             <div className="align-items-center mt-5 mb-5 d-flex justify-content-center">
@@ -16,47 +68,154 @@ const Register = () => {
                                 alt="Fursa Web"
                             />
                         </div>
+                        {/**Employer User Details */}
                         <div className="card-body">
-                            <form>
-                                <label className="form-label" htmlFor="first_name">First Name</label>
+                            <form onSubmit={handleSubmit}>
+
+                                <label className="form-label" htmlFor="username">Username</label>
                                 <input
                                     className="form-control"
-                                    id="first_name"
+                                    id="user.username"
+                                    value={formData.user.username}
+                                    onChange={handleInputChange}
                                     type="text"
                                     required
                                 />
-                                <label className="form-label mt-2" htmlFor="last_name">Last Name</label>
+
+                                <label className="form-label" htmlFor="first_name">First_name</label>
                                 <input
                                     className="form-control"
-                                    id="second_name"
+                                    id="user.first_name"
+                                    value={formData.user.first_name}
+                                    onChange={handleInputChange}
                                     type="text"
                                     required
                                 />
-                                <label className="form-label mt-2" htmlFor="phone">Phone</label>
+
+                                <label className="form-label" htmlFor="last_name">Last_name</label>
                                 <input
                                     className="form-control"
-                                    id="phone_number"
+                                    id="user.last_name"
+                                    value={formData.user.last_name}
+                                    onChange={handleInputChange}
+                                    type="text"
+                                    required
+                                />
+
+                                <label className="form-label" htmlFor="email">Email</label>
+                                <input
+                                    className="form-control"
+                                    id="user.email"
+                                    value={formData.user.email}
+                                    onChange={handleInputChange}
+                                    type="text"
+                                    required
+                                />
+
+                                <label className="form-label" htmlFor="phone_number">Phone_number</label>
+                                <input
+                                    className="form-control"
+                                    id="user.phone_number"
+                                    value={formData.user.phone_number}
+                                    onChange={handleInputChange}
                                     type="tel"
-                                    required
-                                />
-                                <label className="form-label mt-2" htmlFor="email_address">Email Address</label>
-                                <input
-                                    className="form-control"
-                                    id="email_address"
-                                    type="email"
                                     required
                                 />
                                 <label className="form-label mt-2" htmlFor="password">Password</label>
                                 <input
                                     className="form-control"
-                                    id="password"
+                                    id="user.password"
+                                    value={formData.user.password}
+                                    onChange={handleInputChange}
                                     type="password"
-                                    required
                                 />
 
+                                <h2>Company Details</h2>
+                                <label className="form-label" htmlFor="company_name">Company Name</label>
+                                <input
+                                    className="form-control"
+                                    id="company_name"
+                                    value={formData.company_name}
+                                    onChange={handleInputChange}
+                                    type="text"
+                                    required
+                                />
+                                <label className="form-label mt-2" htmlFor="company_description">Brief Description</label>
+                                <textarea
+                                    className="form-control"
+                                    id="company_description"
+                                    value={formData.company_description}
+                                    onChange={handleInputChange}
+                                    required
+                                ></textarea>
+
+
+                                <label className="form-label mt-2" htmlFor="company_type">Employer Type</label>
+                                <select
+                                    className="form-control"
+                                    id="company_type"
+                                    value={formData.company_type}
+                                    onChange={handleInputChange}
+                                    required
+                                >
+                                    <option value='' disable>
+                                        Select Type
+                                    </option>
+
+                                    <option value="NGO">NGO</option>
+                                    <option value="STARTUP">Startup</option>
+                                    <option value="GOVERNMENT">Government</option>
+                                </select>
+                                <label className="form-label mt-2" htmlFor="company_type">Sub County</label>
+                                <select
+                                    className="form-control"
+                                    id="sub_county"
+                                    value={formData.sub_county}
+                                    onChange={handleInputChange}
+                                    required
+                                >
+                                    <option value='' disable>
+                                        Select Sub-county
+                                    </option>
+
+                                    <option value="MVITA">Mvita</option>
+                                    <option value="CHANGAMWE">Changamwe</option>
+                                    <option value="NYALI">Nyali</option>
+                                    <option value="KISAUNI">Kisauni</option>
+                                    <option value="LIKONI">Likoni</option>
+                                    <option value="JOMVU">Jomvu</option>
+                                    <option value="OUTSIDE MOMBASA">Outside Mombasa</option>
+                                </select>
+                                <label className="form-label mt-2" htmlFor="company_phone"> Company Phone</label>
+                                <input
+                                    className="form-control"
+                                    id="company_phone"
+                                    value={formData.company_phone}
+                                    onChange={handleInputChange}
+                                    type="tel"
+                                    required
+                                />
+                                <label className="form-label mt-2" htmlFor="company_email">Company Email </label>
+                                <input
+                                    className="form-control"
+                                    id="company_email"
+                                    value={formData.company_email}
+                                    onChange={handleInputChange}
+                                    type="email"
+                                    required
+                                />
+                                <label className="form-label mt-2" htmlFor="business_number">Business Number</label>
+                                <input
+                                    className="form-control"
+                                    id="company_registration_number"
+                                    value={formData.company_registration_number}
+                                    onChange={handleInputChange}
+                                    type="number"
+                                    required
+                                />
                                 <div className="col-12 mt-4">
                                     <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" id="invalidCheck" required/>
+                                        <input className="form-check-input" type="checkbox" id="invalidCheck" required />
                                         <label className="form-check-label" for="invalidCheck">
                                             I accept the Terms and Conditions
                                         </label>
@@ -82,4 +241,3 @@ const Register = () => {
 }
 
 export default Register;
-
--- src/pages/seeker/register.js
+++ src/pages/seeker/register.js
@@ -1,7 +1,58 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        user: {
+            username: '',
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone_number: '',
+            password: '',
+        },
+        is_in_school: '',
+        level_of_education: '',
+        is_currently_employed: '',
+        is_running_business: '',
+        areas_of_interest: '',
+        other_areas_of_interest: '',
+        referer: '',
+    });
+
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        const fieldId = id.split('.')[0];
+
+        if (fieldId === 'user') {
+            const subFieldId = id.split('.')[1];
+            setFormData((prevFormData) => ({
+                ...prevFormData,
+                user: {
+                    ...prevFormData.user,
+                    [subFieldId]: value,
+                },
+            }));
+        } else {
+            setFormData((prevFormData) => ({
+                ...prevFormData,
+                [id]: value,
+            }));
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            console.log(formData);
+            const response = await axios.post('http://127.0.0.1:8000/api/auth/register/employer/', formData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div>
             <div className="align-items-center mt-5 mb-5 d-flex justify-content-center">
@@ -17,46 +68,169 @@ const Register = () => {
                             />
                         </div>
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={handleSubmit}>
+                                <label className="form-label" htmlFor="username">Username</label>
+                                <input
+                                    className="form-control"
+                                    id="user.username"
+                                    value={formData.user.username}
+                                    onChange={handleInputChange}
+                                    type="text"
+                                    required
+                                />
+
                                 <label className="form-label" htmlFor="first_name">First Name</label>
                                 <input
                                     className="form-control"
-                                    id="first_name"
+                                    id="user.first_name"
+                                    value={formData.user.first_name}
+                                    onchange={handleInputChange}
                                     type="text"
                                     required
                                 />
+
                                 <label className="form-label mt-2" htmlFor="last_name">Last Name</label>
                                 <input
                                     className="form-control"
-                                    id="second_name"
+                                    id="user.last_name"
+                                    value={formData.user.last_name}
+                                    onChange={handleInputChange}
                                     type="text"
                                     required
                                 />
+
                                 <label className="form-label mt-2" htmlFor="phone">Phone</label>
                                 <input
                                     className="form-control"
-                                    id="phone_number"
+                                    id="user.phone_number"
+                                    value={formData.user.phone_number}
+                                    onChange={handleInputChange}
                                     type="tel"
                                     required
                                 />
                                 <label className="form-label mt-2" htmlFor="email_address">Email Address</label>
                                 <input
                                     className="form-control"
-                                    id="email_address"
+                                    id="user.email_address"
+                                    value={formData.user.email_address}
+                                    onChange={handleInputChange}
                                     type="email"
                                     required
                                 />
                                 <label className="form-label mt-2" htmlFor="password">Password</label>
                                 <input
                                     className="form-control"
-                                    id="password"
+                                    id="user.password"
+                                    value={formData.user.password}
+                                    onChange={handleInputChange}
                                     type="password"
                                     required
                                 />
 
+                                <label className="form-label" htmlFor="level_of_education">Level of Education</label>
+                                <select
+                                    className="form-control"
+                                    id="in_school"
+                                    value={formData.in_school}
+                                    onChange={handleInputChange}
+                                    required
+                                >
+                                    <option value='' disabled>
+
+                                    </option>
+                                    <option value="true">True</option>
+                                    <option value="false">False</option>
+                                </select>
+
+                                <label className="form-label" htmlFor="employed">Are you employed?</label>
+                                <select
+                                    className="form-control"
+                                    id="employment"
+                                    value={formData.employmeny}
+                                    onChange={handleInputChange}
+                                    required
+                                >
+                                    <option value='' disabled>
+
+                                    </option>
+                                    <option value="true">True</option>
+                                    <option value="false">False</option>
+                                </select>
+
+                                <label className="form-label" htmlFor="in_business">Are you in business?</label>
+                                <select
+                                    className="form-control"
+                                    id="in_business"
+                                    value={formData.in_business}
+                                    onChange={handleInputChange}
+                                    required
+                                >
+                                    <option value='' disabled>
+
+                                    </option>
+                                    <option value="true">True</option>
+                                    <option value="false">False</option>
+                                </select>
+
+                                <label className="form-label" htmlFor="level_of_education">Level of Education</label>
+                                <select
+                                    className="form-control"
+                                    id="level_of_education"
+                                    value={formData.level_of_education}
+                                    onChange={handleInputChange}
+                                    required
+                                >
+                                    <option value='' disable>
+                                        Select Type
+                                    </option>
+
+                                    <option value="PRIMARY">Pri</option>
+                                    <option value="SECONDARY">Sec</option>
+                                    <option value="College">coll</option>
+                                    <option value="Unversity">Uni</option>
+
+                                </select>
+
+                                <label className="form-label" htmlFor="interest_areas">Areas of Interests</label>
+                                <select
+                                    className="form-control"
+                                    id="interest_area"
+                                    value={formData.interest_area}
+                                    onChange={handleInputChange}
+                                    required
+                                >
+                                    <option value='' disable>
+                                        Select Type
+                                    </option>
+
+                                    <option value="Tvet">TVET</option>
+                                    <option value="Employed">EMPLOYED</option>
+                                    <option value="Businesss">BUSINESS</option>
+                                    <option value="Arts">ARTS</option>
+                                    <option value="Enterpreneur">Enterpreneur</option>
+                                    <option value="Digital">Digital</option>
+                                    <option value="job_search">Job search</option>
+                                    <option value="Health">Health</option>
+                                    <option value="Tourism">Tourism</option>
+
+
+                                </select>
+
+                                <label className="form-label mt-2" htmlFor="last_name">Other areas of interest</label>
+                                <input
+                                    className="form-control"
+                                    id="other_interest"
+                                    value={formData.user.other_interest}
+                                    onChange={handleInputChange}
+                                    type="text"
+                                    required
+                                />
+
+
+
                                 <div className="col-12 mt-4">
                                     <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" id="invalidCheck" required/>
+                                        <input className="form-check-input" type="checkbox" id="invalidCheck" required />
                                         <label className="form-check-label" for="invalidCheck">
                                             I accept the Terms and Conditions
                                         </label>
