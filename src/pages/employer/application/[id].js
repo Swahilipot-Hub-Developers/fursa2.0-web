4ecfd548d55df9311bfa782ec8bc93f989178863
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2024-01-16 16:32:56 +0300

    Build error fix

--- src/pages/employer/application/[id].js
+++ src/pages/employer/application/[id].js
@@ -1,1 +1,10 @@
 // Viewing application by ID
+const Application = () => {
+    return (
+        <div>
+            <p>Displaying single jobs by id</p>
+        </div>
+    )
+}
+
+export default Application;
